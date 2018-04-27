b160d9e3e63ac1ca8c31266c354b50c3f374383d
Mark Vlasenko <mvlasenko@sdl.com>
2018-04-28 02:06:09 +0300

    basic project with auth

---
+++ RemoteUpkeep/Models/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace RemoteUpkeep.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("RemoteUpkeepContext", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public virtual DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Message>()
+                .HasRequired(e => e.User)
+                .WithMany(e => e.Messages)
+                .HasForeignKey(e => e.Id);
+        }
+
+    }
+}
---
+++ RemoteUpkeep/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RemoteUpkeep.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
