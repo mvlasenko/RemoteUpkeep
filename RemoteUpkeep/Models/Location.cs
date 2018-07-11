5a44a6c6c49f33bb159a240d021046606df9a892
Mark Vlasenko <mvlasenko@sdl.com>
2018-07-11 17:44:40 +0300

    google maps editor added

--- RemoteUpkeep/Models/Location.cs
+++ RemoteUpkeep/Models/Location.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.Entity.Spatial;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RemoteUpkeep.Models
 {
@@ -20,8 +20,29 @@ namespace RemoteUpkeep.Models
         [UIHint("MultilineText")]
         public string Description { get; set; }
 
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
         [UIHint("_Geography")]
-        public DbGeography Geography { get; set; }
+        [NotMapped]
+        [Display(Name = "Geography")]
+        public string Geography {
+            get
+            {
+                if (this.Latitude == 0 || this.Longitude == 0)
+                    return null;
+                return string.Format("({0}, {1})", this.Latitude, this.Longitude);
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    this.Latitude = double.Parse(value.Split(',')[0].Trim('(', ')', ' '));
+                    this.Longitude = double.Parse(value.Split(',')[1].Trim('(', ')', ' '));
+                }
+            }
+        }
 
         [Display(Name = "Changed By")]
         public string ChangedByUserId { get; set; }
