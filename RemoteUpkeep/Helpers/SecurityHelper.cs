6c456795049b419664bf897fe71e716d9843b418
Mark Vlasenko <mvlasenko@sdl.com>
2018-07-27 16:21:19 +0300

    lost file

---
+++ RemoteUpkeep/Helpers/SecurityHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+
+namespace RemoteUpkeep.Helpers
+{
+    public static class SecurityHelper
+    {
+        public static bool AdminVisible()
+        {
+            return
+               HttpContext.Current.User != null &&
+               HttpContext.Current.User.Identity.IsAuthenticated &&
+               HttpContext.Current.User.Identity.Name == "mark.vlasenko@gmail.com"; //todo: user type based security
+        }
+    }
+}
