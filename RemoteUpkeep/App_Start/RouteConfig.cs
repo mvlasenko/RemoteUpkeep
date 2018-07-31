be019ab8544f60e595ecb56e7bcc5b145560e7e7
Mark Vlasenko <mvlasenko@sdl.com>
2018-07-31 14:34:14 +0300

    ui fixes

--- RemoteUpkeep/App_Start/RouteConfig.cs
+++ RemoteUpkeep/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace RemoteUpkeep
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Order", action = "Index", id = UrlParameter.Optional }
             );
 
         }
