9fa91625f6ee60f5c906dcfbbc1929c66aba341a
Mark Vlasenko <mvlasenko@sdl.com>
2018-10-12 11:49:10 +0300

    FE localization started

---
+++ RemoteUpkeep/Helpers/TranslationHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RemoteUpkeep.Models;
+using RemoteUpkeep.ViewModels;
+
+namespace RemoteUpkeep.Helpers
+{
+    public static class TranslationHelper
+    {
+        public static List<Service> GetServices()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                List<Service> res = context.Services.ToList();
+
+                var x = 
+                (
+                    from s in context.Services
+                    join t in context.Translations
+                    on s.Id equals t.RecordId
+                    where t.Table == Table.Services && t.Field == Field.Title && t.LanguageId == 5 
+                    select new ServiceViewModel { Id = s.Id, Description = s.Description, Title = t.TranslationValue, CreatedDateTime = s.CreatedDateTime, CreatedByUserId = s.CreatedByUserId }
+                 ).ToList();
+
+                return res;
+            }
+        }
+
+    }
+}
---
+++ RemoteUpkeep/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RemoteUpkeep.ViewModels
+{
+    public class ServiceViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Title", ResourceType = typeof(Properties.Resources))]
+        public string Title { get; set; }
+
+        [UIHint("MultilineText")]
+        [Display(Name = "Description", ResourceType = typeof(Properties.Resources))]
+        public string Description { get; set; }
+
+        [Display(Name = "CreatedBy", ResourceType = typeof(Properties.Resources))]
+        public string CreatedByUserId { get; set; }
+
+        [Display(Name = "Created", ResourceType = typeof(Properties.Resources))]
+        public DateTime CreatedDateTime { get; set; }
+    }
+}
